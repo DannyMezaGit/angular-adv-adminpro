fe012fde1587063bedbf5756f602c3eeb56adc59
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-22 16:32:26 -0700

    servicio settings

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,35 +8,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AccountSettingsComponent implements OnInit {
-  linkTheme = document.querySelector('#theme');
 
-  links: NodeListOf<Element>;
-  constructor() { }
+  
+  constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
-    this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTime();
+   this.settingsService.checkCurrentTime();
   }
 
   changeTheme(theme: string) {
-    const url = `./assets/css/colors/${theme}.css`
-
-    this.linkTheme.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-    this.checkCurrentTime();
+    this.settingsService.changeTheme(theme);
   }
 
-  checkCurrentTime() {
-
-    this.links.forEach(element => {
-      element.classList.remove('working');
-      const btnTheme = element.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
-      const currentTheme = this.linkTheme.getAttribute('href');
-
-      if ( btnThemeUrl === currentTheme ) {
-        element.classList.add('working');
-      }
-    });
-  }
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -5,5 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class SettingsService {
 
-  constructor() { }
+  private linkTheme = document.querySelector('#theme');
+
+  constructor() { 
+    const url = localStorage.getItem('theme') || '/assets/css/colors/purple-dark.css';
+    this.linkTheme.setAttribute('href', url);
+  }
+
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`
+
+    this.linkTheme.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+
+    this.checkCurrentTime();
+    
+  }
+
+  checkCurrentTime() {
+    const links = document.querySelectorAll('.selector');
+
+      links.forEach(element => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme.getAttribute('href');
+
+      if ( btnThemeUrl === currentTheme ) {
+        element.classList.add('working');
+      }
+    });
+  }
 }
