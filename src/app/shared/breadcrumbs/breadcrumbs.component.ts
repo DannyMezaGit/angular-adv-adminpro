b487fde2c858b0267dbb8763ebcff00ac73c1c8c
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-23 22:34:45 -0700

    funcionalidad de breadcrumbs usando observable

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  titulo: string;
+  
+
+  constructor(private router: Router) {
+
+    this.getArgumentosRuta();
+
+  }
+
+  getArgumentosRuta() {
+    this.router.events.pipe(
+
+      filter(event => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null), // Este fitro se hará al resultado del foltro anterior definido
+      map((event: ActivationEnd) => event.snapshot.data),
+
+    ).subscribe(({ titulo }) => {// destructuramos data.titulo entre llaves sólo pedimos titulo
+      this.titulo = titulo;
+      document.title = `AdminPro - ${titulo}`;
+    });
+  }
 
   ngOnInit(): void {
   }
