4a65b51c151f91ff951bee7a418714950319621a
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-23 21:43:57 -0700

    ejemplo de observable()

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -6,11 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent {
 
-  constructor() { }
+  
+  constructor() { 
+    //  $ queremos almacenar
+    // observer es quien emite los valores, cuando termina, cuando da error, qué información está fluyendo a través de el
+    // el observable no se ejecuta hasta que haya una persona suscrita
+    
+    let i:number = -1;
 
-  ngOnInit(): void {
+    const obs$ = new Observable( observer => {
+      const interval = setInterval( () => {
+        
+        i++;
+        observer.next(i);
+
+        if(i === 4 ) {
+          clearInterval(interval);
+          observer.complete();
+        }
+      }, 1000)
+    } );
+
+    obs$.pipe(
+      retry(2)
+    ).subscribe(
+      valor => console.log('Subs: ', valor),
+      error => console.warn('Error: ', error),
+      () => console.info('Obs Terminado')
+    );
+    
   }
+
 
 }
